c637cdc94f88a68c1746c7559ad65d342210bbee
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-03-31 23:11:26 +0530

    added-vehicleView-card

---
+++ src/pages/VehicleView/VehicleView.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function VehicleView() {
+  return (
+    <div>VehicleView</div>
+  )
+}
--- src/route/RouteNav.jsx
+++ src/route/RouteNav.jsx
@@ -1,8 +1,9 @@
 import AdminCar from "../pages/AdminCar/AdminCar"
 import AdminAction from '../pages/AdminAction/AdminAction'
 import AllCustomers from "../pages/AllCustomers/AllCustomers";
+import VehicleView from "../pages/VehicleView/VehicleView";
 
-
+import { MdOutlineCarCrash } from "react-icons/md";
 import { IoCarSportSharp } from "react-icons/io5";
 import { FaUserTie } from "react-icons/fa";
 import { FaUsers } from "react-icons/fa6";
@@ -29,6 +30,13 @@ const routesNav = [
         path:"/customers",
         component:<AllCustomers />,
         icon:<FaUsers/>
+    },
+    {
+        name:"Vehicle View",
+        key:"vehicleView",
+        path:"/vehicleView",
+        component:<VehicleView />,
+        icon:<MdOutlineCarCrash/>
     },
 ];
 
